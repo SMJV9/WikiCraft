3555bdcce43e56b37696dd3458d9f0b022821d66
SMJV9 <a22311022@uthermosillo.edu.mx>
2024-01-21 22:07:43 -0700

    prueba del git que este funcionando

--- api/WikiCraft/database/migrations/2024_01_18_055827_update_items_table.php
+++ api/WikiCraft/database/migrations/2024_01_18_055827_update_items_table.php
@@ -7,9 +7,7 @@ use PHPUnit\Framework\Attributes\After;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+   
     public function up(): void
     {
         Schema::table('items', function (Blueprint $table) {
@@ -17,9 +15,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::table('items', function (Blueprint $table) {
