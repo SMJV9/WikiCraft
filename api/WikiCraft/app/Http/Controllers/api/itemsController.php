bbfdd73037854d027aa3be25ff968611405465e7
SMJV9 <a22311022@uthermosillo.edu.mx>
2024-01-25 10:23:13 -0700

    se creo la tercera api de items, con esta ya se creo la de biomes, blocks y items

--- api/WikiCraft/app/Http/Controllers/api/itemsController.php
+++ api/WikiCraft/app/Http/Controllers/api/itemsController.php
@@ -1,11 +1,89 @@
 <?php
 
 namespace App\Http\Controllers\api;
-
+use App\Models\Item;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class itemsController extends Controller
 {
-    //
-}
+    public function list(){
+        $items = Item::all();
+
+        $list = [];
+
+        foreach($items as $item){
+            $objet =[
+
+                "id" => $item->id,
+                "name" => $item->name,
+                "image" => $item->image,
+                "stack"=>$item->stack,
+                "created_at" => $item->created_at,
+                "updated_at" => $item->updated_at
+            ];
+            array_push($list, $objet);
+        }
+        return response()->json($list);
+    }
+   public function item($id){
+    $item = Item::where('id','=',$id)->first();
+
+    $objet =[
+
+        "id" => $item->id,
+        "name" => $item->name,
+        "image" => $item->image,
+        "stack"=>$item->stack,
+        "created_at" => $item->created_at,
+        "updated_at" => $item->updated_at
+    ];
+    return response()->json($objet);
+    }
+    public function create(Request $request){
+     $data = $request->validate([
+        'name' => 'requiered|min:3',
+        'stack' => 'requiered|min:1|max"64'
+     ]);   
+     $item = Item::create([
+        'name'=> $data['name'],
+        'stack'=> $data['stack']
+    ]);
+     if($item){
+        $objet =[
+            "response"=>'Sucess. Item saved correctly.',
+            "data"=> $item
+        ];
+        return response()->json($objet);
+    } else {
+        $objet =[
+            "response"=> 'Error: Something went wrong, please try again.', 
+      ];
+        return response()->json($objet);
+    }
+    }
+    public function update(Request $request){
+        $data = $request->validate([
+          'id'=>'required|min:1',
+            'name' => 'required|min:3',
+            'stack' => 'required|min:1|max:64'
+        ]);
+          $item = Item::where("id","=", $data['id'])->first();
+  
+          $item->name=$data['name'];
+          $item->stack=$data['stack'];
+  
+          if($item->update()){
+              $objet =[
+                  "response"=>'Sucess. Item update successfully.',
+                  "data"=> $item
+              ];
+              return response()->json($objet);
+          } else {
+              $objet =[
+                  "response"=> 'Error: Something went wrong, please try again.', 
+            ];
+              return response()->json($objet);
+          }
+      }
+  }
--- api/WikiCraft/routes/api.php
+++ api/WikiCraft/routes/api.php
@@ -3,6 +3,7 @@
 // use App\Http\Controllers\api\BiomesController;
 use App\Http\Controllers\api\biomesController;
 use App\Http\Controllers\api\BlocksController;
+use App\Http\Controllers\api\ItemsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,13 +21,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+// API de biomes
 Route::get('/biomes',[BiomesController::class,'List']);
 Route::get('/biomes/{id}',[BiomesController::class,'item']);
 Route::post('/biomes/create',[BiomesController::class,'create']);
 Route::post('/biomes/{id}/update',[BiomesController::class,'update']);
-
+// API de blocks
 Route::get('/blocks',[BlocksController::class,'List']);
 Route::get('/blocks/{id}',[BlocksController::class,'item']);
 Route::post('/blocks/create',[BlocksController::class,'create']);
 Route::post('/blocks/{id}/update',[BlocksController::class,'update']);
+// API de items
+Route::get('/items',[ItemsController::class,'List']);
+Route::get('/items/{id}',[ItemsController::class,'item']);
+Route::post('/items/create',[ItemsController::class,'create']);
+Route::post('/items/{id}/update',[ItemsController::class,'update']);
